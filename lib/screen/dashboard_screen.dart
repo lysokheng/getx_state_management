214723da8a19d6fc154c0042c1cd9bf3b16a7f6c
Lysokheng <lysokheng.huot@gmail.com>
2022-08-18 13:04:26 +0700

    add LiquidPullToRefresh widget to dashboard screen to make a refresh the page

--- lib/screen/dashboard_screen.dart
+++ lib/screen/dashboard_screen.dart
@@ -1,56 +1,116 @@
+import 'dart:async';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_state_management/controller/dashboard_controller.dart';
+import 'package:getx_state_management/screen/detail_screen.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
-class DashBoardScreen extends StatelessWidget {
+class DashBoardScreen extends StatefulWidget {
   DashBoardScreen({Key? key}) : super(key: key);
-  final controller = Get.put(DashboardController());
+
+  @override
+  State<DashBoardScreen> createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  DashboardController controller = Get.put(DashboardController());
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<LiquidPullToRefreshState> _refreshIndicatorKey =
+      GlobalKey<LiquidPullToRefreshState>();
+
+  Future<void> _handleRefresh() {
+    final Completer<void> completer = Completer<void>();
+    Timer(const Duration(seconds: 1), () {
+      completer.complete();
+    });
+
+    return completer.future.then<void>((_) {
+      controller.fetchNews();
+      setState(() {});
+      ScaffoldMessenger.of(_scaffoldKey.currentState!.context).showSnackBar(
+        SnackBar(
+          content: const Text('Refresh complete'),
+          action: SnackBarAction(
+            label: 'RETRY',
+            onPressed: () {
+              _refreshIndicatorKey.currentState!.show();
+            },
+          ),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('News'),
-        ),
-        body: GetBuilder<DashboardController>(
-          builder: (controller) {
-            if (controller.newsResModel == null && controller.isLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return ListView.builder(
+      appBar: AppBar(
+        title: const Text('News'),
+      ),
+      body: GetBuilder<DashboardController>(
+        builder: (controller) {
+          if (controller.newsResModel == null && controller.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return LiquidPullToRefresh(
+            onRefresh: _handleRefresh,
+            child: ListView.builder(
                 itemCount: controller.newsResModel!.results!.length,
                 itemBuilder: (context, index) {
                   var data = controller.newsResModel!.results![index];
-                  return Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        data.imageUrl != null
-                        ? Image.network(
-                            data.imageUrl.toString())
-                        : Container(),
-                         Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            data.title.toString(),
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
+                  return GestureDetector(
+                    onTap: () {
+                      Get.to(DetailScreen(
+                          title: data.title!,
+                          subTitle: data.description!,
+                          imgURL: data.imageUrl));
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            data.imageUrl != null
+                                ? CachedNetworkImage(
+                                    imageUrl: data.imageUrl!,
+                                    progressIndicatorBuilder: (context, url,
+                                            downloadProgress) =>
+                                        CircularProgressIndicator(
+                                            value: downloadProgress.progress),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                  )
+                                : Container(),
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text(
+                                data.title.toString(),
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text(
+                                data.description.toString(),
+                                style: const TextStyle(
+                                    fontSize: 12, color: Colors.grey),
+                              ),
+                            ),
+                          ],
                         ),
-                         Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            data.description.toString(),
-                            style: TextStyle(fontSize: 12, color: Colors.grey),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   );
-                });
-          },
-        ));
+                }),
+          );
+        },
+      ),
+    );
   }
 }
